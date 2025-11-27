da520ddf1c8e21f97fed8bf6c71f6362803ec559
Kacper Kraus <kacperkraus2425@gmail.com>
2025-11-27 10:02:02 +0100

    Add custom InsufficientFundsException class

---
+++ ExceptionHandlingDemo/ExceptionHandling.Core/InsufficientFundsException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandling.Core
+{
+    public class InsufficientFundsException : Exception
+    {
+        // Standardowy konstruktor
+        public InsufficientFundsException()
+        {
+        }
+
+        // Konstruktor przyjmujący wiadomość (Message)
+        public InsufficientFundsException(string message)
+            : base(message)
+        {
+        }
+
+        // Konstruktor przyjmujący wiadomość i innerException (gdy chcemy zawinąć inny wyjątek)
+        public InsufficientFundsException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
